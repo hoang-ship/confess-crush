f2c3a4298cc5cc4c40f1d5c26328d3d4a4e4e256
hoang-ship <huyhoang125609@gmail.com>
2024-10-16 19:21:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "gửi vợ",
+    introTitle: 'Vợ Linh',
+    introDesc: `chúc vợ anh 20/10 vui vẻ nha `,
+    btnIntro: '^^ấn vô đây đi bé^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'bé có thích món quà này không <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'cho a xem zu và loz bé mỗi ngày được không <33333333',
+    mess: 'anh biết mà 🥰. Yêu vợ nhiều nhiều 😘😘',
+    messDesc: '20/10 vui vẻ nha bé.',
+    btnAccept: 'chào mừng vợ đến với thế giới của hạnh phúc <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
